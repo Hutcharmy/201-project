723c77eedb85be247ffd5f4285b71fe59c13e411
Michael Gentile <Michael Gentile>
2017-03-10 17:48:11 -0500

    all is fucked

---
+++ 201Project/src/ConnectFour.java
@@ -0,0 +1,11 @@
+import javax.swing.*;
+import java.awt.*;
+public class ConnectFour extends JFrame{
+	public ConnectFour(){
+	}
+	public static void main(System[] args){
+		JFrame frame= new ConnectFour();
+		frame.setVisible(true);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+}
