90946bcf2fe00d118665213f5c4da4bdf4c5676b
thughes44 <hughest6@miamioh.edu>
2017-04-19 16:03:41 -0400

    score printing
    added gui elements to print score to playPanel and scorePanel.  Also
    added functionality to the clear button on scorePanel

--- 201Project/src/PlayPanel.java
+++ 201Project/src/PlayPanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -28,6 +29,22 @@ public class PlayPanel extends JLayeredPane {
 		backgroundImage.setBounds(0, 0, 825, 850);
 		this.add(backgroundImage, JLayeredPane.PALETTE_LAYER);
 		
+		playerScore=Score.getScore(AIDifficulty, true);
+		AIScore=Score.getScore(AIDifficulty, false);
+		
+		JLabel printPlayerScore = new JLabel(Integer.toString(playerScore));
+		printPlayerScore.setBounds(308, 16, 75, 75);
+		printPlayerScore.setFont(new Font("Serif", Font.BOLD, 70));
+		printPlayerScore.setForeground(Color.BLACK);
+		//printPlayerScore.setOpaque(true);
+		this.add(printPlayerScore, JLayeredPane.DRAG_LAYER);
+		
+		JLabel printAIScore = new JLabel(Integer.toString(AIScore));
+		printAIScore.setBounds(713, 16, 75, 75);
+		printAIScore.setFont(new Font("Serif", Font.BOLD, 70));
+		printAIScore.setForeground(Color.BLACK);
+		this.add(printAIScore, JLayeredPane.DRAG_LAYER);
+		
 		panel=new BoardPanel(this);
 		panel.setLocation(50, 115);
 		panel.setColors("yellow");
@@ -37,8 +54,6 @@ public class PlayPanel extends JLayeredPane {
 		AIDifficulty=0;
 		AI=new AILogic(AIDifficulty);
 		logic= new GameLogic(AI);
-		playerScore=Score.getScore(AIDifficulty, true);
-		AIScore=Score.getScore(AIDifficulty, false);
 		
 		JButton backButton=new JButton("");
 		backButton.setBounds(300,720,200,61);
--- 201Project/src/ScorePanel.java
+++ 201Project/src/ScorePanel.java
@@ -1,4 +1,5 @@
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -23,17 +24,89 @@ public class ScorePanel extends JLayeredPane {
 		backgroundImage.setBounds(0, 0, 825, 850);
 		this.add(backgroundImage);
 		
+		int[] scoreArray = Score.scoreArray();
+		
+		JLabel easyWins = new JLabel(Integer.toString(scoreArray[0]));
+		easyWins.setBounds(200, 410, 75, 75);
+		easyWins.setFont(new Font("Serif", Font.BOLD, 70));
+		easyWins.setForeground(Color.BLACK);
+		//printPlayerScore.setOpaque(true);
+		this.add(easyWins, JLayeredPane.DRAG_LAYER);
+		
+		JLabel easyLosses = new JLabel(Integer.toString(scoreArray[1]));
+		easyLosses.setBounds(530, 410, 75, 75);
+		easyLosses.setFont(new Font("Serif", Font.BOLD, 70));
+		easyLosses.setForeground(Color.BLACK);
+		this.add(easyLosses, JLayeredPane.DRAG_LAYER);
+		
+		JLabel medWins = new JLabel(Integer.toString(scoreArray[2]));
+		medWins.setBounds(200, 480, 75, 75);
+		medWins.setFont(new Font("Serif", Font.BOLD, 70));
+		medWins.setForeground(Color.BLACK);
+		//printPlayerScore.setOpaque(true);
+		this.add(medWins, JLayeredPane.DRAG_LAYER);
+		
+		JLabel medLosses = new JLabel(Integer.toString(scoreArray[3]));
+		medLosses.setBounds(530, 480, 75, 75);
+		medLosses.setFont(new Font("Serif", Font.BOLD, 70));
+		medLosses.setForeground(Color.BLACK);
+		this.add(medLosses, JLayeredPane.DRAG_LAYER);
+		
+		JLabel hardWins = new JLabel(Integer.toString(scoreArray[4]));
+		hardWins.setBounds(200, 550, 75, 75);
+		hardWins.setFont(new Font("Serif", Font.BOLD, 70));
+		hardWins.setForeground(Color.BLACK);
+		//printPlayerScore.setOpaque(true);
+		this.add(hardWins, JLayeredPane.DRAG_LAYER);
+		
+		JLabel hardLosses = new JLabel(Integer.toString(scoreArray[5]));
+		hardLosses.setBounds(530, 550, 75, 75);
+		hardLosses.setFont(new Font("Serif", Font.BOLD, 70));
+		hardLosses.setForeground(Color.BLACK);
+		this.add(hardLosses, JLayeredPane.DRAG_LAYER);
+		
+		JLabel easy = new JLabel("E");
+		easy.setBounds(362, 406, 75, 75);
+		easy.setFont(new Font("Serif", Font.BOLD, 70));
+		easy.setForeground(Color.BLACK);
+		this.add(easy, JLayeredPane.DRAG_LAYER);
+		
+		JLabel medium = new JLabel("M");
+		medium.setBounds(351, 480, 75, 75);
+		medium.setFont(new Font("Serif", Font.BOLD, 70));
+		medium.setForeground(Color.BLACK);
+		this.add(medium, JLayeredPane.DRAG_LAYER);
+		
+		JLabel hard = new JLabel("H");
+		hard.setBounds(357, 550, 75, 75);
+		hard.setFont(new Font("Serif", Font.BOLD, 70));
+		hard.setForeground(Color.BLACK);
+		this.add(hard, JLayeredPane.DRAG_LAYER);
+		
+		JButton clearButton = new JButton("");
+		clearButton.setBounds(145,673,240,73);
+		clearButton.setOpaque(true);
+		
+		clearButton.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Score.resetScores();
+				frame.changeContentPane(new MainJPanel(frame));
+				frame.changeContentPane(new ScorePanel(frame));
+			}
+		});
+		
+		this.add(clearButton);
+		this.setVisible(true);
 		
 		JButton backButton=new JButton("");
 		backButton.setBounds(453,673,208,73);
 		backButton.setOpaque(false);
 		
-		System.out.println("We here");
 		backButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				frame.changeContentPane(new MainJPanel(frame));
-				System.out.println("Shitty");
 			}
 		});
 		this.add(backButton);
